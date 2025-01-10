d3a9ade56ce4485d14d8ebbb352c8a6db8101103
kadekwira <kadekw668@gmail.com>
2025-01-10 21:12:41 +0800

    seed

--- arsip-backend/database/seeders/DatabaseSeeder.php
+++ arsip-backend/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use App\Models\Pegawai;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,27 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Admin::create([
+            'nip' => '123456789', // Ganti dengan NIP yang sesuai
+            'id_admin' => 1, // ID Admin yang unik
+            'nama' => 'Admin Example',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password123'), // Password di-hash menggunakan bcrypt
+            'username' => 'adminuser',
+            'bidang' => 'kadis',
+            'status' => 'aktif',
+        ]);
+
+        // Membuat data Pegawai
+        Pegawai::create([
+            'nip' => '987654321', // Ganti dengan NIP yang sesuai
+            'id_admin' => 2, // ID Admin yang unik
+            'nama' => 'Pegawai Example',
+            'email' => 'pegawai@example.com',
+            'password' => Hash::make('password123'), // Password di-hash menggunakan bcrypt
+            'username' => 'pegawaiuser',
+            'bidang' => 'staff',
+            'status' => 'aktif',
         ]);
     }
 }
